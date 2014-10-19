d7e387ad0f7f5a3af261631a155acaa726af625f
talha276 <talhashoaib27@gmail.com>
2014-10-19 19:17:13 -0400

    test

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SmartCampus::Application.config.secret_key_base = 'fb7696f4d48a77a1495340210f671f88ab0ea1fc253ca398eaeb1f80dd0e8a5025cb811b331cccf7d672c2a9e716bedf0c8ecd92e476aae5e48af7c1fc959b6e'
+SmartCampus::Application.config.secret_key_base = '1419ea1d081a2c6e87be9847f306e6ccd301dcddba99743c1024f5109f05af3498a2c6676520e6424184f85eadfd3c68fc234d2a8a3dcd64be364aad6aef9b17'
