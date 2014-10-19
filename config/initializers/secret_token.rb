6576635b8679353517aa6b0a67816ac0b073e329
talha276 <talhashoaib27@gmail.com>
2014-10-19 18:27:19 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmartCampus::Application.config.secret_key_base = 'fb7696f4d48a77a1495340210f671f88ab0ea1fc253ca398eaeb1f80dd0e8a5025cb811b331cccf7d672c2a9e716bedf0c8ecd92e476aae5e48af7c1fc959b6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmartCampus::Application.config.session_store :cookie_store, key: '_smart_campus_session'
