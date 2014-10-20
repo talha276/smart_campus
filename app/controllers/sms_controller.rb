d3f9cca820b2f8950b2afbdbc3720a9b17ef40f7
talha276 <talhashoaib27@gmail.com>
2014-10-19 22:47:07 -0400

    sms-two

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,11 +1,17 @@
 class SmsController < ApplicationController
   def index
+  	@sms = Sms.all
   end
 
   def edit
   end
 
   def show
+  end
+
+  def create
+  	s = SMS.new(params[:sms])
+  	s.save!
   end
 
   def delete
